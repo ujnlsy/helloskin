55925fea46441259d22293c640d343d6c634faae
siyuan <ujnlsy@126.com>
2018-08-21 22:55:34 +0800

    request 请求封装好了

--- src/api/api.js
+++ src/api/api.js
@@ -3,17 +3,16 @@
  */
 import {
   wxRequest
-} from '@/utils/wxRequest';
+} from '../utils/wxRequest';
 
-let env = "test" //dev 或者 test
-let apiSkin = 'https://helloskin.club/'
-let debugParse = ''
+const env = "test" //dev 或者 test
+//const apiSkin = 'https://helloskin.club/'
+const apiSkin = 'http://localhost:9006/'
 
-if (env == 'test')
-{
-  apiSkin = 'http://localhost:9006/'
-  debugParse = '.do'
-}
 
 //获取用户第一次提交的信息
-const getCustomerFirstInfo = (params) => wxRequest(params, apiSkin + 'api/consult/list/get' + debugParse);
+const getCustomerFirstInfo = (params) => wxRequest(params, apiSkin + 'api/consult/list/get');
+
+export default {
+  getCustomerFirstInfo
+}
--- src/utils/wxRequest.js
+++ src/utils/wxRequest.js
@@ -10,20 +10,43 @@ import tip from './tip'
 // const TIMESTAMP = util.getCurrentTime()
 // const SIGN = md5.hex_md5((TIMESTAMP + API_SECRET_KEY).toLowerCase())
 
-const wxRequest = async(params = {}, url) => {
-  tip.loading();
+// const wxRequest = async(params = {}, url) => {
+//   tip.loading();
+//   let data = params.query || {};
+//   // data.sign = SIGN;
+//   // data.time = TIMESTAMP;
+//   let res = await wepy.request({
+//     url: url,
+//     method: params.method || 'GET',
+//     data: data,
+//     header: { 'Content-Type': 'application/json' },
+//   });
+//   tip.loaded();
+//   console.log('wxR',res)
+//   return res;
+// };
+
+const wxRequest = function (params = {}, url) {
   let data = params.query || {};
-  // data.sign = SIGN;
-  // data.time = TIMESTAMP;
-  let res = await wepy.request({
-    url: url,
-    method: params.method || 'GET',
-    data: data,
-    header: { 'Content-Type': 'application/json' },
-  });
-  tip.loaded();
-  return res;
-};
+  const promise = new Promise((resolve,reject)=>{
+    wx.request({
+      url:url,
+      data:data,
+      mehtod:params.method || 'GET',
+      success:function(res){
+        resolve(res)
+      },
+      fail:function(res){
+        reject(res);
+      },
+      complete:function(){
+        console.log('complete');
+      }
+    })
+  })
+  
+  return promise
+}
 
 
 module.exports = {
